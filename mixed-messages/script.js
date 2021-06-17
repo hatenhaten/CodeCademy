2ff12732e18d841eaae9df09711422cc1485f3e2
Kallum Tolkien <kallumtolkien@gmail.com>
2021-06-17 15:25:59 +0100

    final commit on daily wisdom generator

--- mixed-messages/script.js
+++ mixed-messages/script.js
@@ -1,18 +1,39 @@
-const quotes = {
-    list1: [ 'Remember to focus on all my tasks like meditation - light touch focus - Sustainable focus', 'Courage, Wisdom, Justice, Temperance', 'Live as if you were living for the second time and you had acted as foolishly the first time as you are about to act now'],
-    list2: ['Since you make good or evil by your own thoughts, it is your own thoughts that ought to be your principal concern', 'Make sure a task is complete before moving on', 'Set a Pure and Clear Intention to give focused attention to each task before and during it'],
-    list3: ['Practice the art of interpreting situations constructively', 'Manage what you can\'t influence', 'Listen & Read actively - constantly summarising in own words']
+// generate a random number
+const randomNumberGenerator = (num) => Math.floor(Math.random() * num);
+
+// create a collection of wisdom as an object of arrays
+const collectedWisdom = {
+    communication: [ 'Be conscious in switching between system 1 and 2 where appropriate', 'Practice the art of interpreting all communication constructively', 'Listen & Read actively - constantly summarising in own words', 'Sync breathing rythm with the other person', 'Mirror body language', 'Hold good eye contact'],
+    effectiveness: [ 'Live as if you were living for the second time and you had acted as foolishly the first time as you are about to act now', 'Courage, Wisdom, Justice, Temperance - The 4 Stoic virtues', 'Remember to focus on all my tasks like meditation - light touch focus - Sustainable focus', 'Manage what you can\'t influence',  'Make sure a task is complete before moving on', 'Set a Pure and Clear Intention to give focused attention to each task before and during it'],
+    wellbeing: ['Since you make good or evil by your own thoughts, it is your own thoughts that ought to be your principal concern', 'Practice Enjoying the present moment no matter what it is' ]
 };
 
-const randomNumberGenerator = () => Math.floor(Math.random() * 3);
+// store given wisdom in an array
+const givenWisdom = [];
 
-const quote1 =  quotes.list1[randomNumberGenerator()];
-const quote2 =  quotes.list2[randomNumberGenerator()];
-const quote3 =  quotes.list3[randomNumberGenerator()];
+// iterate over the collectedWisdom object
+for(let prop in collectedWisdom) {
+    let optionIdx = randomNumberGenerator(collectedWisdom[prop].length);
 
-const quoteString = `${quote1},
-${quote2} &
-${quote3}`;
+    // use the object's properties to customize the message added to personal wisdom with each iteration
+    switch(prop) {
+        case 'communication' :
+            givenWisdom.push(`To improve your communication remember: ${collectedWisdom[prop][optionIdx]}`);
+            break;
+        case 'effectiveness' :
+            givenWisdom.push(`To improve your effectiveness remember: ${collectedWisdom[prop][optionIdx]}`);
+            break;
+        case 'wellbeing' :
+            givenWisdom.push(`To improve your wellbeing remember: ${collectedWisdom[prop][optionIdx]}`);
+            break;
+        default:
+            givenWisdom.push('There is no wisdom to be found here.')
+    } 
+};
 
+function formatWisdom(wisdom) {
+    const formatted = wisdom.join('\n');
+    console.log(formatted);
+};
 
-console.log(quoteString);
+formatWisdom(givenWisdom);
